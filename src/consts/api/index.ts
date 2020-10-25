fac9b076a588042e52a227ee846c6cd5b50968b7
asim.dahal <asim.dahal@joinsage.com>
2020-10-25 20:15:01 +0545

    chore: spelling corrected

--- src/consts/api/index.ts
+++ src/consts/api/index.ts
@@ -3,13 +3,13 @@ import { IPackage } from "../../interfaces/IPackage";
 export const NPM_SEARCH = "https://api.npms.io/v2/search?q=";
 export const PACKAGE_INFO = "https://api.npms.io/v2/package/mget";
 
-export const getPackageInformation = (dependencis: string[]) => {
+export const getPackageInformation = (dependencies: string[]) => {
   return fetch(`${PACKAGE_INFO}`, {
     method: "POST",
     headers: {
       "content-type": "application/json",
     },
-    body: JSON.stringify(dependencis),
+    body: JSON.stringify(dependencies),
   }).then((response) => response.json()) as Promise<{
     [key: string]: { code?: string; collected: IPackage };
   }>;
